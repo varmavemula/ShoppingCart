545533202264c64d81dbfc251c20c54052248e66
vemula muni varma <munimunnavemula123@gmail.com>
2023-10-25 22:02:20 +0530

    button actions are created using arrowfunctions

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,8 +9,31 @@ class CartItem extends React.Component{
             price:999,
             Qnt:1,
             img:''
-        }
+        }  
+    }
+    increaseQuantity = () => {
+
+        //setState form-I --> uses shallow merge --> that means, update the items in setstate object to the this.state.
+        this.setState({
+            Qnt: this.state.Qnt+1
+        });
+
+        //setState form-II --> used when prevState is needed.
+        // this.setState((prevState)=>{
+        //     return {
+        //     Qnt:prevState.Qnt+1
+        //     }
+        // });
+    }
+    decreaseQuantity = () =>{
+        if(this.state.Qnt>0){
+        this.setState({
+            Qnt: this.state.Qnt-1
+        });}
         
+    }
+    deleteProduct = () => {
+        console.log("deleted");
     }
     render () {
         return (
@@ -26,9 +49,21 @@ class CartItem extends React.Component{
                     <div>QTY: {this.state.Qnt}</div>
                     <div className="cart-item-actions">
                        { /*Buttons*/}
-                       <img alt="increase" className="action-icons" src="https://banner2.cleanpng.com/20180610/eau/kisspng-plus-and-minus-signs-clip-art-5b1d241b5e4aa4.7318136215286364433862.jpg"/>
-                       <img alt="decrease" className="action-icons" src="https://banner2.cleanpng.com/20180425/dow/kisspng-plus-and-minus-signs-subtraction-plus-minus-sign-m-minus-5ae07899248a12.0382096915246603771497.jpg"/>
-                       <img alt="delete" className="action-icons" src="https://e7.pngegg.com/pngimages/836/813/png-clipart-web-application-computer-icons-delete-icon-game-text.png"/>
+                       <img 
+                            alt="increase" 
+                            className="action-icons" 
+                            src="https://banner2.cleanpng.com/20180610/eau/kisspng-plus-and-minus-signs-clip-art-5b1d241b5e4aa4.7318136215286364433862.jpg"
+                            onClick={this.increaseQuantity}/>
+                       <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://banner2.cleanpng.com/20180425/dow/kisspng-plus-and-minus-signs-subtraction-plus-minus-sign-m-minus-5ae07899248a12.0382096915246603771497.jpg"
+                            onClick={this.decreaseQuantity}/>
+                       <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://e7.pngegg.com/pngimages/836/813/png-clipart-web-application-computer-icons-delete-icon-game-text.png"
+                            onClick={this.deleteProduct}/>
 
                     </div>
                 </div>
